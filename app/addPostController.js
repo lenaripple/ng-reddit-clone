9b69b33713ab73e9fcbd07b45eb520c6c2f4c449
Lena Ripple <ripplelena@gmail.com>
2017-05-17 16:12:21 -0600

    validation added

---
+++ app/addPostController.js
@@ -0,0 +1,18 @@
+angular.module('app')
+  .controller('addPostController',
+    function() {
+      const vm = this
+      vm.$onInit = function () {
+      vm.posts = [
+        {title:"Nice", author:"lirip", image: "http://www.telegraph.co.uk/content/dam/Travel/galleries/travel/activityandadventure/The-worlds-most-beautiful-mountains/mountains-Kirkjufe_3374110a-large.jpg", description: "Kirkjufell, Iceland"},
+      ]
+  }
+  vm.addPost = function(){
+    vm.posts.push(vm.post)
+    delete vm.post
+  }
+  vm.deletePost = function (e, post) {
+    e.preventDefault()
+    vm.posts.splice(vm.posts.indexOf(post), 1)
+  }
+  })
---
+++ app/app.js
@@ -0,0 +1,5 @@
+angular.module('app', [])
+  .component('app', {
+    controller: 'addPostController',
+    templateUrl: './templates/postTemplate.html'
+  })
