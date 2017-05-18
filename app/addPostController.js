9d872c1471b7b544c87d0fcafc33416e0b87067c
Lena Ripple <ripplelena@gmail.com>
2017-05-18 14:22:53 -0600

    times adjusted

--- app/addPostController.js
+++ app/addPostController.js
@@ -2,10 +2,11 @@ angular.module('app')
   .controller('addPostController',
     function() {
       const vm = this
+      const votes = 0
       vm.$onInit = function () {
       vm.posts = [
-        {title:"Nice", author:"lirip", image: "http://www.telegraph.co.uk/content/dam/Travel/galleries/travel/activityandadventure/The-worlds-most-beautiful-mountains/mountains-Kirkjufe_3374110a-large.jpg", description: "Kirkjufell, Iceland", date: '', votes: 12, comments:[]},
-        {title:"Cool post", author:"other", image: "https://apod.nasa.gov/apod/image/1505/AuroraNorway_Richardsen_1080.jpg", description: "This is super cool wow omg amazing", date: '', votes: 4, comments: []}
+        {title:"Nice", author:"lirip", image: "http://www.telegraph.co.uk/content/dam/Travel/galleries/travel/activityandadventure/The-worlds-most-beautiful-mountains/mountains-Kirkjufe_3374110a-large.jpg", description: "Kirkjufell, Iceland", date: '5/17/17 2:40 PM', votes: 12, comments:[]},
+        {title:"Cool post", author:"other", image: "https://apod.nasa.gov/apod/image/1505/AuroraNorway_Richardsen_1080.jpg", description: "This is super cool wow omg amazing", date: '5/17/17 2:21 PM', votes: 4, comments: []}
       ]
   }
   vm.addPost = function(){
@@ -20,7 +21,7 @@ angular.module('app')
     vm.posts.splice(vm.posts.indexOf(post), 1)
   }
   vm.changeVotes = function(post, changeVal) {
-    console.log('clicked');
     post.votes += changeVal;
+    vm.votes
     }
   })
